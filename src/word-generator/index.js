02d517f837d76a8170033b7d001641216a3a88a0
Sanath Samarasinghe <sanath@serrexlabs.com>
2020-02-19 07:02:50 +0530

    task completions

---
+++ src/word-generator/index.js
@@ -0,0 +1,30 @@
+/**
+ * Importing the extended methods
+ */
+const { getRandomWordWithIndexSync, getRandomWordWithIndex } = require('./word-maker-extended');
+
+const wordsGeneratorSync = ({max = 1, withErrors = false, printFizzBuzz = false } = {}) => {
+    const wordsWithIndex = [];
+    for (let index=0; index < (max + 1); index++) {
+        wordsWithIndex.push(getRandomWordWithIndexSync({ index, withErrors, printFizzBuzz }));
+    }
+
+    return wordsWithIndex;
+};
+
+
+
+const wordsGenerator = async ({max = 1, withErrors = false, printFizzBuzz = false } = {}) => {
+    const wordsWithIndex = [];
+    for (let index=0; index < (max + 1); index++) {
+        wordsWithIndex.push(getRandomWordWithIndex({ index, withErrors, printFizzBuzz, slow: true }));
+    }
+
+    return Promise.all(wordsWithIndex);
+};
+
+
+module.exports = {
+    wordsGeneratorSync,
+    wordsGenerator
+};
---
+++ src/word-generator/word-maker-extended.js
@@ -0,0 +1,57 @@
+/**
+ * Extending random words generators
+ */
+const { getRandomWordSync, getRandomWord } = require('word-maker');
+
+
+const getRandomWordWithIndexSync = ({index, withErrors = false, printFizzBuzz = false }) => {
+    try {
+        const wordFizzBuzz = fizzBuzzGenerator(index);
+
+        if (!printFizzBuzz || !wordFizzBuzz) {
+            return `${index}: ${getRandomWordSync({withErrors})}`;
+        }
+
+        return  `${index}: ${wordFizzBuzz}`;
+    } catch (e) {
+        return `${index}: It shouldn't break anything!`
+    }
+};
+
+
+const getRandomWordWithIndex = async ({index, withErrors = false, printFizzBuzz = false, slow = false }) => {
+    try {
+        const wordFizzBuzz = fizzBuzzGenerator(index);
+
+        if (!printFizzBuzz || !wordFizzBuzz) {
+            return `${index}: ${await getRandomWord({withErrors, slow})}`;
+        }
+
+        return  `${index}: ${wordFizzBuzz}`;
+    } catch (e) {
+        return `${index}: It shouldn't break anything!`
+    }
+};
+
+const fizzBuzzGenerator = (index) => {
+
+    if (index % 15 === 0) {
+        return "FizzBuZZ";
+    }
+
+    if (index % 5 === 0) {
+        return "BuZZ";
+    }
+
+    if (index % 3 === 0) {
+        return "Fizz";
+    }
+
+    return false;
+};
+
+module.exports = {
+    getRandomWordWithIndexSync,
+    getRandomWordWithIndex
+};
+
